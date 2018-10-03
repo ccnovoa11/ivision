e035df741b52beff5a7b00373a1dc34764906614
Camilo <ca.anzola@uniandes.edu.co>
2018-10-03 10:05:36 -0500

    Place picker para crear circulo

--- iVision/app/src/main/java/com/example/mac/ivision/MainActivity.java
+++ iVision/app/src/main/java/com/example/mac/ivision/MainActivity.java
@@ -4,12 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tv_registrar;
-
+    Button ingresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,16 @@ public class MainActivity extends AppCompatActivity {
             {
                 Intent intentRegistrar = new Intent(MainActivity.this, Registro.class);
                 MainActivity.this.startActivity(intentRegistrar);
+            }
+        });
+
+        ingresar = findViewById(R.id.button);
+        ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intentIngresar = new Intent(MainActivity.this, NuevoCirculoInicio.class);
+                MainActivity.this.startActivity(intentIngresar);
             }
         });
     }
---
+++ iVision/app/src/main/java/com/example/mac/ivision/NuevoCirculoInicio.java
@@ -0,0 +1,58 @@
+package com.example.mac.ivision;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+import android.content.Intent;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.location.places.Place;
+
+public class NuevoCirculoInicio extends AppCompatActivity
+{
+
+    TextView edText;
+    int PLACE_PICKER_REQUEST = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_circulo_inicio);
+
+        edText = findViewById(R.id.geoPoint);
+        edText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+                //Intent intentMaps = new Intent(NuevoCirculoInicio.this, MapsActivity.class);
+                //NuevoCirculoInicio.this.startActivity(intentMaps);
+
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+                try
+                {
+                   startActivityForResult(builder.build(NuevoCirculoInicio.this), PLACE_PICKER_REQUEST);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode==PLACE_PICKER_REQUEST)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                Place place = PlacePicker.getPlace(NuevoCirculoInicio.this, data);
+                edText.setText(place.getAddress());
+            }
+        }
+    }
+}
